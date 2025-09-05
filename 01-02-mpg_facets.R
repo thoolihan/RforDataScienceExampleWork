42ddea3672bedb83ae27896ebd6b83f42b474a40
Tim Hoolihan <tim@hoolihan.net>
2025-09-05 10:26:49 -0400

    Removing file since moved to notebook

--- 01-02-mpg_facets.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-ggplot(data = ggplot2::mpg) +
-  geom_point(mapping = aes(x = displ, y = hwy)) + 
-  stat_density2d(mapping = aes(x = displ, y = hwy)) + 
-  facet_wrap(~ class, nrow = 2)
- 
